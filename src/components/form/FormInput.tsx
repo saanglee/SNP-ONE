12e40abcb3b197e76cf7aa82bd42b72dbcf3140f
manjyuriah <manjyuriah01@gmail.com>
2022-07-24 18:09:06 +0900

    feat input입력 후 submit시 콘솔에 데이터 확인

--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,66 +1,45 @@
 import React from "react";
-import { Input, InputLabel, TextField, FormControl } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Controller } from "react-hook-form";
 
 interface InputProps {
   title: string;
   placeholder?: string;
-  onKeyUp?: React.KeyboardEvent;
-  onChange?: React.ChangeEvent;
-  inputRef?: any;
   inputProps?: any;
+  name?: any;
+  control?: any;
 }
 
 const FormInput = ({
-  title,
   placeholder,
-  inputRef,
   inputProps,
+  name,
+  control,
+  title,
 }: InputProps) => {
   const [value, setValue] = React.useState("");
   const inputsRef = React.useRef<HTMLInputElement | any>(null);
-
-  const regexp = {
-    korean: /[a-z0-9]|[ [\]{}()<>?|`~!@#$%^&*-_+=,.;:"'\\]/g,
-    email: /^([0-9a-zA-Z_.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/,
-  };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(value);
     setValue((event.target as HTMLInputElement).value);
   };
 
-  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log(inputRef.current.value);
-    if (title === "이름") {
-      if (regexp.korean.test(event.target.value)) {
-        event.target.value = event.target.value.replace(regexp.korean, "");
-      }
-    } else if (title === "생년월일" || title === "연락처") {
-      event.target.value = event.target.value
-        .replace(/[^0-9.]/g, "")
-        .replace(/(\..*)\./g, "$1");
-    }
-  };
-  React.useEffect(() => {
-    if (inputRef.current.value === "") {
-      // console.log("활성화");
-    }
-  }, [value]);
-
   return (
-    <FormControl sx={{ mb: 3 }}>
-      <TextField
-        id="standard-password-input"
-        label={title}
-        type="text"
-        autoComplete="current-password"
-        variant="standard"
-        onChange={handleChange}
-        onKeyUp={handleKeyUp}
-        value={value}
-        placeholder={placeholder}
-        inputRef={inputRef}
-        inputProps={inputProps}
-      />
-    </FormControl>
+    <Controller
+      name={name}
+      defaultValue=""
+      control={control}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          label={title}
+          type="text"
+          variant="standard"
+          placeholder={placeholder}
+          inputProps={inputProps}
+        />
+      )}
+    />
   );
 };
 export default FormInput;
