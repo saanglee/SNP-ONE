e08bcefdde01f618881078184b102a5470577696
dev-redo <mis05041@naver.com>
2022-07-25 15:42:52 +0900

    feat: add suspense

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
 import Dashboard from "../pages/Dashboard";
 import SignUp from "../pages/SignUp";
@@ -8,21 +9,24 @@ import { theme } from "../styles/theme";
 import { RecoilRoot } from "recoil";
 import NotFound404 from "../pages/NotFound404";
 import About from "../pages/About";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <RecoilRoot>
-        <ThemeProvider theme={theme}>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/dash" element={<Dashboard />} />
-            <Route path="/about" element={<About />} />
-            {["/", "/sign"].map((path, index) => {
-              return <Route path={path} element={<SignUp />} key={index} />;
-            })}
-            <Route path="*" element={<NotFound404 />} />
-          </Routes>
-        </ThemeProvider>
+        <Suspense fallback={<div>로딩중</div>}>
+          <ThemeProvider theme={theme}>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/dash" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+              {["/", "/sign"].map((path, index) => {
+                return <Route path={path} element={<SignUp />} key={index} />;
+              })}
+              <Route path="*" element={<NotFound404 />} />
+            </Routes>
+          </ThemeProvider>
+        </Suspense>
       </RecoilRoot>
     </BrowserRouter>
   );
