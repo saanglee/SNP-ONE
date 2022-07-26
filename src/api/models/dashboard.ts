bb5cb557f5dffc7c39e7c1c3a705b7bdf1cd46a8
minkim <manjyuriah01@gmail.com>
2022-07-26 13:59:50 +0900

    feat 더미유저 데이터 경로 수정

--- src/api/models/dashboard.ts
+++ src/api/models/dashboard.ts
@@ -2,9 +2,9 @@ import { apiRequest } from "../instance/instance";
 import { Applicant } from "../../types/datshboard";
 
 export const getApplicantData = async () =>
-  await apiRequest.get<Applicant[]>("/user");
+  await apiRequest.get<Applicant[]>("/users");
 
 export const patchIsApplicantChecked = async ({ id, isChecked }: Applicant) =>
-  await apiRequest.patch("/user", id, {
+  await apiRequest.patch("/users", id, {
     isChecked: !isChecked,
   });
