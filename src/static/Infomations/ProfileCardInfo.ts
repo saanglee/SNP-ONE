290b50b3edea6456aea4544ef1deb55009bf562f
matty255 <89088205+matty255@users.noreply.github.com>
2022-07-27 10:24:37 +0900

    add: card

--- src/static/Infomations/ProfileCardInfo.ts
+++ src/static/Infomations/ProfileCardInfo.ts
@@ -29,9 +29,9 @@ export const ProfileCardInfo = [
   },
   {
     name: "이혜림",
-    called: "10자 이내로",
-    desc: "무엇 무엇을 맡았습니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등 무엇 무엇을 맡았습니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등니다 등",
+    called: "UI & Interactions",
+    desc: "MUI, Lottie, styled-components, css3",
     profilePicture:
-      "https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      "https://static.wixstatic.com/media/2cd43b_012bc245a911403b8219a7f68da95837~mv2.png/v1/fill/w_300,h_350,q_90/2cd43b_012bc245a911403b8219a7f68da95837~mv2.png",
   },
 ];
