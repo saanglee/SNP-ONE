3735e7fabbe70c6b3214dd9d32577fcabc9319bd
Sangji Lee <sangllee20@gmail.com>
2022-07-27 11:23:17 +0900

    fix: csv download logic

--- src/components/dashboard/ListHeader.tsx
+++ src/components/dashboard/ListHeader.tsx
@@ -1,18 +1,13 @@
-import { useRecoilValue } from "recoil";
-import styled from "styled-components";
-import { listOnTargetPage } from "../../store/dashboard";
 import "./dashboard.css";
 import ExcelDownloadBtn from "./ExcelDownloadBtn";
 
-const ListHeader = () => {
-  const applicantsList = useRecoilValue(listOnTargetPage);
-  console.log(applicantsList);
+const ListHeader = ({ items }: any) => {
   return (
     <div className="ListHeader">
       <header>
         AI학습용 교통 데이터 수집을 위한 <br /> 크라우드 워커 지원 현황
       </header>
-      <ExcelDownloadBtn applicantsList={applicantsList} />
+      <ExcelDownloadBtn applicantsList={items} />
     </div>
   );
 };
